d55966178d76f00a3bc8833a2895fae655d9a4c9
jwells80 <jewells1980@gmail.com>
2020-01-22 19:23:20 -0600

    Change to js templates

---
+++ assets/scripts/app.js
@@ -0,0 +1,7 @@
+$(function () {
+    $('head').load("/includes/header.html");
+    $('#navigation').load("/includes/nav.html");
+    $('#banner').load('/includes/banner.html')
+    $('#footer').load("/includes/footer.html");
+
+  });
