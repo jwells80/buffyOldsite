674638f6627fb2287df602063803ae2ff7e58f20
Jason Wells <jewells1980@gmail.com>
2023-08-04 20:48:19 -0500

    audio player install

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,25 @@
-$(function () {
-    $('head').load("/includes/header.html");
-    $('#navigation').load("/includes/nav.html");
-    $('#banner').load('/includes/banner.html')
-    $('#footer').load("/includes/footer.html");
+// $(function () {
+//     $('head').load("/includes/header.html");
+//     $('#navigation').load("/includes/nav.html");
+//     $('#banner').load('/includes/banner.html')
+//     $('#footer').load("/includes/footer.html");
 
-  });
+//   });
+
+getAndSetHtml = (selector, file) => {
+    fetch(file).then(function(response) {
+        return response.text();
+    }).then(function(body) {
+        document.querySelector(selector).innerHTML = body
+    })
+}
+init = () => {
+    getAndSetHtml('head', 'includes/header.html')
+    getAndSetHtml('#navigation', "includes/nav.html")
+    getAndSetHtml('#banner', 'includes/banner.html')
+    getAndSetHtml('#footer', "includes/footer.html")
+    getAndSetHtml('#audio-player', "includes/audio-player.html")
+
+}
+
+init()
